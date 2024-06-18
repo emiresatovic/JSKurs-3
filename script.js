77ec75f8f9293e5883fc7179d39fb8186d24ccd1
Emir Esatović <165719085+emiresatovic@users.noreply.github.com>
2024-06-18 15:18:55 +0200

    const sabiranje.

---
+++ script.js
@@ -0,0 +1,20 @@
+function klikni_me(a,b) {
+    console.log(a+b);
+    
+
+}
+
+
+klikni_me(200, 30);
+
+
+
+
+// tradicionlani nacin
+
+const klikni_me_nova = a => console.log(a + 15);
+
+
+klikni_me_nova(50);
+
+
