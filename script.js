a0a6dd3bd7ff4f133e4f6e7d32f1160c1046ce7c
Emir Esatović <165719085+emiresatovic@users.noreply.github.com>
2024-06-18 16:50:07 +0200

    events!

--- script.js
+++ script.js
@@ -1,20 +1,76 @@
-function klikni_me(a,b) {
-    console.log(a+b);
-    
 
+
+/*
+
+const klikni = (dugme) => {
+    console.log(dugme.innerText);
 }
 
 
-klikni_me(200, 30);
+let btns = document.querySelectorAll('button');
+
+
+btns.forEach(function (btn) {
+    // event listener koji ceka za klik, za svaki button.
+   btn.addEventListener('click', function(event) {
+    let clicked_btn = event.target;
+
+    console.log(clicked_btn.innerText);
+   });
+});
+
+
+let link = document.querySelector('#link');
+link.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(event.target);
+
+})
+
+
+let form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    console.log('test');
+});
+
+*/
+
+let opcije = document.querySelector('select');
+opcije.addEventListener('change',(event) => {
+    console.log(event.target.value);
+
+});
+
+
+window.addEventListener('resize', () => {
+    if(window.innerWidth > 1000) {
+        console.log('prozor veci od 1000');
+    } else {
+        console.log('prozor manji od 1000');
+    }
+});
+
+let input = document.querySelector('input');
+
+input.addEventListener("keydown", (event) => {
+    if(event.key === "Delete") {
+        alert('obrisano');
+
+    }
+})
 
 
 
+/*
+btn.addEventListener('click', function() {
+    console.log('kliknuo');
+});
 
-// tradicionlani nacin
-
-const klikni_me_nova = a => console.log(a + 15);
+*/
 
 
-klikni_me_nova(50);
 
 
